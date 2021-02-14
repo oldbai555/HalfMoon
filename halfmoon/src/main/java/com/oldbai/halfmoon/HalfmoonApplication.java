5f6729cdba913ada79fb2cd78cbbe65afed98ac7
老白 <1005777562@qq.com>
2021-02-14 13:00:54 +0800

    完成项目的初始化构建。

---
+++ halfmoon/src/main/java/com/oldbai/halfmoon/HalfmoonApplication.java
@@ -0,0 +1,13 @@
+package com.oldbai.halfmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfmoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalfmoonApplication.class, args);
+    }
+
+}
---
+++ halfmoon/src/test/java/com/oldbai/halfmoon/HalfmoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldbai.halfmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalfmoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
