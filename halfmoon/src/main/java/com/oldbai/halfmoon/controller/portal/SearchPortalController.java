ca1a3b5be8b3bf39e2c1142f96b7ea0ca41f16d5
老白 <1005777562@qq.com>
2021-02-23 12:03:06 +0800

    完成项目的初始化构建。 进行全局初始化，详情参看构建笔记。 用户中心：初始化管理员账号，获取图灵验证码，获取邮箱验证码，注册，检查用户名是否注册，检查邮箱是否注册 开始进行Solr

--- halfmoon/src/main/java/com/oldbai/halfmoon/controller/portal/SearchPortalController.java
+++ halfmoon/src/main/java/com/oldbai/halfmoon/controller/portal/SearchPortalController.java
@@ -1,4 +1,14 @@
 package com.oldbai.halfmoon.controller.portal;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/portal/search")
+@CrossOrigin
+@RestController
+@Api("门户-搜索")
 public class SearchPortalController {
+
 }
